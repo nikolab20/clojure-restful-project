230462e54f820aec10c499235fa8e60be011a1c1
Nikola Bakic <nikola.bakic20@hotmail.com>
2021-01-27 22:33:01 +0100

    Frontend and backend app created

---
+++ backend/clojure-restful-project-backend/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-restful-project-backend
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ backend/clojure-restful-project-backend/test/clojure_restful_project_backend/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-restful-project-backend.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-restful-project-backend.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
