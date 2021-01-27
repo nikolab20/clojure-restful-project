1048f28cd8fcc46bbe9fbec121761be0aafc7b5e
Nikola Bakic <nikola.bakic20@hotmail.com>
2021-01-27 22:40:50 +0100

    Project dependencies added

--- backend/clojure-restful-project-backend/project.clj
+++ backend/clojure-restful-project-backend/project.clj
@@ -1,10 +1,15 @@
 (defproject clojure-restful-project-backend "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [prismatic/schema "1.1.12"]
+                 [metosin/compojure-api "2.0.0-alpha30"]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [toucan "1.15.1"]
+                 [mysql/mysql-connector-java "8.0.12"]
+                 [buddy/buddy-hashers "1.7.0"]]
   :main ^:skip-aot clojure-restful-project-backend.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:uberjar {:aot :all}})
